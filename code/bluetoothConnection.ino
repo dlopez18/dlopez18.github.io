14b0646c94a9a0075f5c407ab9059bf185c800ec
dlopez18 <89310605+dlopez18@users.noreply.github.com>
2025-04-24 15:45:55 -0500

    Update bluetoothConnection.ino

--- code/bluetoothConnection.ino
+++ code/bluetoothConnection.ino
@@ -1,1 +1,24 @@
+#include <SoftwareSerial.h>
 
+SoftwareSerial mySerial(TxD of Bluetooth Module, RxD of Bluetooth Module);
+
+void setup() {
+	//	Open serial communications and wait for port
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  while (!Serial) {
+  ;
+  }
+  Serial.println("Start Communication");
+}
+
+void loop() {
+  if (mySerial.available()) {
+  	Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+  	mySerial.write(Serial.read());
+  }
+}
+  
+ 
